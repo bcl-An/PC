b3d689c2282591cbf70bcde47989af690c04641e
bcl-An <72387207+bcl-An@users.noreply.github.com>
2020-10-06 18:50:53 +0800

    Update PC.java

--- PC.java
+++ PC.java
@@ -3,9 +3,13 @@ package test;
 public class PC {
     private String pinpai;
     private int time;
-    PC(String abc, int time){
+    PC(String pinpai, int time){
         this.pinpai = pinpai;
-        this.time = time;
+        if (time>2020){
+            this.time=2020;
+        }else {
+            this.time = time;
+        }
     }
     CPU cpu;
     HardDisk hd;
@@ -16,7 +20,8 @@ public class PC {
         this.hd = hd;
     }
     public void show(){
-        System.out.println("CPU speed"+cpu.getSpeed());
-        System.out.println("HardDisk amount"+hd.getAmount());
+        System.out.println("brand:"+pinpai+" time:"+time);
+        System.out.println("CPU speed:"+cpu.getSpeed());
+        System.out.println("HardDisk amount:"+hd.getAmount());
     }
 }
